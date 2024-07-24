05c86acf7050a2c525ab3857ec94a1f87fc04470
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-23 22:36:55 -0400

    set up the constructors shape and subclasses tri,circ,sq and worked on the index.js

--- index.js
+++ index.js
@@ -2,32 +2,62 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
-inquirer
-.prompt([
+
+// set this up very similarly to mini project 09, 
+//but changed the promises for async so that the function doesn't tie up the entire document while it runs
+
+const prompt = async () => {
+    const answers = await inquirer.prompt([
         {
             type: 'input',
             name: 'text',
             message: 'Enter up to three characters for the logo text',
-         },
+        },
         {
             type: 'input',
             name: 'textColor',
             message: 'Enter a color keyword or hexadecimal number for the logo text',
         },
         {
-            type: 'list', 
+            type: 'list',
             name: 'shape',
             message: 'Select a shape for the logo',
             choices: ['circle', 'triangle', 'square'],
-         },
+        },
         {
             type: 'input',
-            name: 'shapecolor',
+            name: 'shapeColor',
             message: 'Enter a color keyword or hexadecimal number for the shape color',
         },
     ])
-.then((answers) => {
-    const
-})
 
-should make this async instead of a promise
+
+const { text, textColor, shape, shapeColor } = answers;
+let shapeSVG;
+
+switch (shape) {
+    case 'circle':
+        shapeSVG = new Circle();
+        break;
+    case 'triangle':
+        shapeSVG = new Triangle();
+        break;
+    case 'square':
+        shapeSVG = new Square();
+        break;
+}
+
+shapeSVG.setColor(shapeColor);
+
+const svgContent = `
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  ${shapeSVG.render()}
+  <text x="150" y="125" font-size="48" text-anchor="middle" fill="${textColor}">${text}</text>
+</svg>
+`;
+
+fs.writeFileSync('logo.svg', svgContent);
+  console.log('Generated logo.svg');
+};
+
+prompt();
